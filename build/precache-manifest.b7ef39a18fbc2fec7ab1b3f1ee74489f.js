d6cfcee707962a329ea1c125bd4bc83df80abf6f
hemical01 <56999378+hemical01@users.noreply.github.com>
2021-11-10 10:37:47 +0100

    first commit

---
+++ build/precache-manifest.b7ef39a18fbc2fec7ab1b3f1ee74489f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3426a95ed071854fe02810d90badf151",
+    "url": "/index.html"
+  },
+  {
+    "revision": "564d6883f1f6b6392e6b",
+    "url": "/static/css/main.233cbc38.chunk.css"
+  },
+  {
+    "revision": "3f98d4bbc3b932d740ba",
+    "url": "/static/js/2.ddf526ee.chunk.js"
+  },
+  {
+    "revision": "564d6883f1f6b6392e6b",
+    "url": "/static/js/main.2589c24c.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
